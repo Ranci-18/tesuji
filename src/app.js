ccf229536eb73d742492997a3d3859b80406acc8
Frank <wanyoike39@gmail.com>
2024-03-19 07:44:32 +0300

    dark mode fix

--- src/app.js
+++ src/app.js
@@ -1,8 +1,23 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Home from './components/Home';
 import './app.scss';
 
 export default function App() {
+  useEffect(() => {
+    function toggleTheme () {
+      const body = document.querySelector('body');
+      const currentTheme = body.getAttribute('data-theme');
+      const newTheme = currentTheme === 'light' ? 'dark' : 'light';
+      
+      body.setAttribute('data-theme', newTheme);
+  }
+    const toggleBtn = document.querySelector('#toggleButton');
+    toggleBtn.addEventListener('click', toggleTheme);
+    
+    return () => {
+      toggleBtn.removeEventListener('click', toggleTheme);
+    };
+  }, [])
   return (
     <div className='app'>
       <label className='switch'>
